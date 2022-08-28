35f50601f3621b4a415e935489f1684f45d20b49
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2022-08-28 02:49:02 +0000

    Add structure of password generator

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#value");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+/**
+ * Create password
+ */
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"
+
+let newPassword ="";
+
+sizePassword.innerHTML = sliderElement.ariaValueMax;
+
