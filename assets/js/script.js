0c18158046ad7b03d010f44daeae2380a558242b
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2022-08-28 04:03:42 +0000

    Add function copy to Clipboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
 /**
- * Create password
+ * Characters
  */
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"
 
@@ -24,4 +24,31 @@ slider.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
+/**
+ * Function to generate and create password
+ */
+
+function generatePassword() {
+
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+      
+    }
+    
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    newPassword = pass
+}
+
+/**
+ *  Create a function copy to clipboard
+ */
+
+function copyPassword() {
+    alert("Password copied successfully!")
+    navigator.clipboard.writeText(newPassword);
+
+}
 
